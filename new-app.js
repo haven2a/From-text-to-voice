5db1aade6377d9d088be8de5fd03d407738eb433
Hacen atek <HACENATEK41@GMAIL.COM>
2025-02-03 20:32:19 +0100

    Update new-app.js

--- new-app.js
+++ new-app.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
 // تحميل متغيرات البيئة من ملف .env
 require('dotenv').config();
 
