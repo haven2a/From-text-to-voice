de6b48026093a9e3c156e3b295c35befccd07ee9
Hacen atek <HACENATEK41@GMAIL.COM>
2025-02-05 16:44:14 +0100

    Update new-app.js

--- new-app.js
+++ new-app.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// إضافة رؤوس Content Security Policy للسماح بتحميل الأنماط والخطوط من Google Fonts
+// تعديل سياسة Content Security Policy للسماح بتحميل الخطوط من Google Fonts
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
